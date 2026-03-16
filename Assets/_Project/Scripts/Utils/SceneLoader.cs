675c17c1626fa4fbe3499d65e34b283b17e80d6b
HAIDANG205 <l3eatcmnr@gmail.com>
2026-03-16 17:06:22 +0700

    menu

--- Assets/_Project/Scripts/Utils/SceneLoader.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine.SceneManagement;
-
-public static class SceneLoader
-{
-    public static void LoadMainMenu() => SceneManager.LoadScene("MainMenu");
-    public static void LoadLobby() => SceneManager.LoadScene("Lobby");
-    public static void LoadGame() => SceneManager.LoadScene("Game");
-}
